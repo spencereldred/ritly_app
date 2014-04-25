44616ada963b62f78c80b52aa7de32a06456f3b4
spencereldred <eldredspencer@gmail.com>
2014-04-25 07:12:42 -0700

    Add urls controller, add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 RitlyApp::Application.routes.draw do
-  root to: 'ritly#index'
+  get "urls/index"
+  
+  root to: 'urls#index'
 
-  get '/go/:random_string/preview', to: 'ritly#preview', as: :preview
+  get '/go/:random_string/preview', to: 'urls#preview', as: :preview
 
-  get '/go/:random_string', to: 'ritly#go', as: :go
+  get '/go/:random_string', to: 'urls#go', as: :go
 
-  get '/ritly/:id/show', to: 'ritly#show', as: :show
+  get '/urls/:id/show', to: 'urls#show', as: :show
 
-  post '/ritly', to: 'ritly#create', as: :create 
+  post '/urls', to: 'urls#create', as: :create 
 
 #   Spencers-MacBook-Pro-2:ritly_app Spencer$ rake routes
 #  Prefix Verb URI Pattern                          Controller#Action
